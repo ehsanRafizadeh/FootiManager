2534cee71abf1515e50f49d191620eac22d8ff17
ehsan rafizadeh <you@example.com>
2019-11-10 16:23:19 +0000

    DB connected

--- UserManagerService/index.js
+++ UserManagerService/index.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./models');
 const handle = require('./handlers');
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/',(req,res)=>{
     res.send('hello world')
 });
 
-app.use(handle.notFound);
 
+app.use(handle.notFound);
 app.use(handle.errors);
+
+
 
 
 app.listen(port,console.log('Server started on port',port));
